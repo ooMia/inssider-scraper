dbb18d495d7d18971d17506e77e1a4ff8093410d
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2025-05-10 02:18:10 +0900

    ✨ Feat : Follow

--- model/repository/user.py
+++ model/repository/user.py
@@ -1,39 +1,72 @@
 from typing import Optional
 
-from sqlalchemy import BigInteger, Boolean, Column, ForeignKey, String
+from sqlalchemy import BigInteger, Boolean, Column, DateTime, ForeignKey, String, func
 from sqlalchemy.orm import DeclarativeBase, relationship
+
+
+def users_id_fk():
+    return ForeignKey("users.id", ondelete="CASCADE")
+
+
+class TimestampMixin:
+    # fmt: off
+    created_at = Column(DateTime, default=func.now(), doc="생성 시간")
+    updated_at = Column(DateTime, default=func.now(), onupdate=func.now(), doc="수정 시간")
+    # fmt: on
 
 
 class Base(DeclarativeBase):
     pass
 
 
-class User(Base):
+class Follow(Base, TimestampMixin):
+    __tablename__ = "follows"
+
+    # fmt: off
+    from_user_id = Column(BigInteger, users_id_fk(), primary_key=True, doc="팔로우를 요청한 사용자 ID")
+    to_user_id   = Column(BigInteger, users_id_fk(), primary_key=True, doc="팔로우 요청 대상의 사용자 ID")
+    # fmt: on
+
+
+class User(Base, TimestampMixin):
     __tablename__ = "users"
 
     # fmt: off
-    id       = Column(BigInteger, primary_key=True, autoincrement=True)
-    email    = Column(String(255), immutable=True, unique=True)
-    password = Column(String(255))  # 해싱된 비밀번호
-    salt     = Column(String(255))  # 비밀번호 해싱용 salt
+    id            = Column(BigInteger, primary_key=True, autoincrement=True)
+    email         = Column(String(255), unique=True, doc="이메일 주소")
+    password      = Column(String(255), doc="비밀번호 해싱값")
+    password_salt = Column(String(255), doc="비밀번호 해싱용 salt")
     # fmt: on
 
     details = relationship(
-        "UserDetail", back_populates="user", uselist=False, cascade="all, delete-orphan"
+        "UserDetail",
+        back_populates="user",
+        cascade="all, delete-orphan",  # User 삭제 시, UserDetail도 함께 삭제됨
+        uselist=False,  # 1:1 관계
+        info={"doc": "User와 UserDetail의 1:1 관계"},
+    )
+
+    following = relationship(
+        "User",
+        secondary="follows",  # M:N 관계
+        primaryjoin=id == Follow.from_user_id,
+        secondaryjoin=id == Follow.to_user_id,
+        backref="followers",
+        info={"doc": "User 간의 팔로우를 나타내는 M:N 관계"},
     )
 
 
-class UserDetail(Base):
+class UserDetail(Base, TimestampMixin):
     __tablename__ = "user_details"
 
     # fmt: off
-    user_id      = Column(BigInteger, ForeignKey("users.id", ondelete="CASCADE"), primary_key=True)
-    username     = Column(Optional(String(255)))  # 사용자 이름
-    introduction = Column(Optional(String(255)))  # 자기소개
-    profile_url  = Column(Optional(String(255)))  # 프로필 사진 URL
+    user_id      = Column(BigInteger, users_id_fk(), primary_key=True, doc="users 테이블의 PK")
+    username     = Column(String(255), nullable=True, doc="사용자 이름")
+    introduction = Column(String(255), nullable=True, doc="자기소개")
+    profile_url  = Column(String(255), nullable=True, doc="프로필 사진 URL")
 
-    account_visibility   = Column(Boolean, default=True)  # 프로필 공개 여부
-    followers_visibility = Column(Boolean, default=True)  # 팔로워 목록 공개 여부
+    account_visibility   = Column(Boolean, default=True, doc="프로필 공개 여부")
+    followers_visibility = Column(Boolean, default=True, doc="팔로워 목록 공개 여부")
     # fmt: on
 
     user = relationship("User", back_populates="details")
